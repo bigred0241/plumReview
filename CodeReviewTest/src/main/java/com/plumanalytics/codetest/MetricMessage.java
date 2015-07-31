1e77fe3c94cd440c56b3be63f65e7c27167a8b05
John Blackman <bigred0241@gmail.com>
2015-07-31 11:54:34 -0400

    expecting API-level documentation for public methods that aren't basic getters/setters

--- CodeReviewTest/src/main/java/com/plumanalytics/codetest/MetricMessage.java
+++ CodeReviewTest/src/main/java/com/plumanalytics/codetest/MetricMessage.java
@@ -6,5 +6,8 @@ import java.text.ParseException;
  * Implemented by concrete message objects
  */
 public interface MetricMessage {
+
+  //expecting some API-level documentation here     
+	//(same for all public methods that are not basic getters/setters)
   public void init(String line) throws ParseException;
 }
